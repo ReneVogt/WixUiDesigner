84ad543af465a7044e804b44ceb6fb20dfea843d
Rene Vogt <rvogt@aki-gmbh.com>
2021-07-25 16:32:58 +0200

    reorganize repo and recreate vsix project with better template

--- src/WixUiDesigner/Properties/AssemblyInfo.cs
+++ src/WixUiDesigner/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
-using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WixUiDesigner")]
-[assembly: AssemblyDescription("Design support for WiX UI fragments.")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WixUiDesigner")]
@@ -29,6 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-
-[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
