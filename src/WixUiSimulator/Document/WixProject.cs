4ff31084dcb95459fe79d89581b39414390e149f
Rene Vogt <rvogt@aki-gmbh.com>
2021-08-13 15:21:02 +0200

    refactoring

--- src/WixUiSimulator/Document/WixProject.cs
+++ src/WixUiSimulator/Document/WixProject.cs
@@ -6,14 +6,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using EnvDTE;
-using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.Threading;
 using WixUiSimulator.Logging;
-using Task = System.Threading.Tasks.Task;
 
 #nullable enable
 
@@ -21,26 +16,20 @@ namespace WixUiSimulator.Document
 {
     sealed class WixProject
     {
-        static readonly Dictionary<Project, WixProject> projects = new Dictionary<Project, WixProject>();
-        static DTE2? dte;
+        static readonly Dictionary<Project, WixProject> projects = new ();
 
         internal Project Project { get; }
 
         internal WixProject(Project project) => Project = project;
 
-        internal static async Task InitializeAsync(IServiceProvider serviceProvider, JoinableTaskFactory joinableTaskFactory, CancellationToken cancellationToken)
-        {
-            await joinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            dte = (DTE2?)serviceProvider?.GetService(typeof(SDTE));
-        }
-        internal static WixProject? Get(string fileName)
+        internal static WixProject? Get(ProjectItem projectItem)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
-            var project = dte?.Solution?.FindProjectItem(fileName)?.ContainingProject;
+            var project = projectItem.ContainingProject;
             if (project is null) return null;
             if (!Guid.TryParse(project.Kind, out var guid) || guid != Defines.WixProjectKindGuid)
             {
-                Logger.Log(DebugContext.Document, $"{fileName} is part of project {project.Name} which is not a WiX project (kind: {project.Kind}).");
+                Logger.Log(DebugContext.Document, $"{projectItem.Document.FullName} is part of project {project.Name} which is not a WiX project (kind: {project.Kind}).");
                 return null;
             }
             if (projects.TryGetValue(project, out var wixProject)) return wixProject;
